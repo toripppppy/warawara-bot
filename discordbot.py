43f46177e00c4df3bb26f2980a6f454c8c28b794
toripppppy <96613082+toripppppy@users.noreply.github.com>
2021-12-24 22:12:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,36 @@
 from discord.ext import commands
 from os import getenv
 import traceback
-
-bot = commands.Bot(command_prefix='/')
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+import discord
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+client = discord.Client()
 
 
+@client.event
+async def on_message(message: discord.Message):
+    # メッセージの送信者がbotだった場合は無視する
+    if message.author.bot:
+        return
+
+    if message.content == "!join":
+        if message.author.voice is None:
+            await message.channel.send("あなたはボイスチャンネルに接続していません。")
+            return
+        # ボイスチャンネルに接続する
+        await message.author.voice.channel.connect()
+        await message.channel.send("接続しました。")
+
+    elif message.content == "!leave":
+        if message.guild.voice_client is None:
+            await message.channel.send("接続していません。")
+            return
+
+        # 切断する
+        await message.guild.voice_client.disconnect()
+
+        await message.channel.send("切断しました。")
+
+        
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
